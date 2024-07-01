d169f10e62b443ef1af3f35c4b54c453b43d7979
kyawswar <aungkyawswar75@gmail.com>
2024-07-01 23:26:26 +0630

    first commit

---
+++ backend/controllers/RecipeController.js
@@ -0,0 +1,32 @@
+const Recipe = require("../models/Recipe");
+
+const RecipeController = {
+    index : (req,res) => {
+        return res.json({ msg : 'get all recipes'})
+    },
+    store : async(req,res) => {
+        try {
+            const { title, description, ingredients } = req.body;
+
+            const recipe = await Recipe.create({
+              title,
+              description,
+              ingredients,
+            });
+            return res.json(recipe);
+        } catch (e) {
+            return res.status(400).json({msg : 'invalid fields'})
+        }
+    },
+    show : (req,res) => {
+        return res.json({ msg : 'show single recipe'})
+    },
+    destroy : (req,res) => {
+        return res.json({ msg : 'delete single recipe'})
+    },
+    update : (req,res) => {
+        return res.json({ msg : 'edit single recipe'})
+    },
+}
+
+module.exports = RecipeController;
---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const morgan = require("morgan");
+const recipeRoutes = require("./routes/recipes");
+const { mongoose } = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+const mongoURL =
+  "mongodb+srv://aungkyawswartun:test1234@recipes-cluster.fwbmrdn.mongodb.net/?retryWrites=true&w=majority&appName=Recipes-Cluster";
+mongoose.connect(mongoURL).then(() => {
+  console.log("connected to db");
+  app.listen(process.env.PORT, () => {
+    console.log("app is running on port : " + process.env.PORT);
+  });
+});
+
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  return res.json({ hello: "world" });
+});
+
+app.use("/api/recipes", recipeRoutes);
